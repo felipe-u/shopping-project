aa157790fe834a7854894db714e48af8df02aec2
felipe-u <felipe_uv@outlook.com>
2024-08-30 21:46:55 -0500

    Using some "magic" association methods for retrieving data per user

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,16 +13,18 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
-  })
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+      userId: req.user.id,
+    })
     .then((result) => {
       // console.log(result);
       console.log("Created Product!");
-      res.redirect('/admin/products');
+      res.redirect("/admin/products");
     })
     .catch((err) => {
       console.log(err);
@@ -35,8 +37,11 @@ exports.getEditProduct = (req, res, next) => {
     res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then((product) => {
+  req.user
+    .getProducts({ where: { id: prodId } })
+    // Product.findByPk(prodId)
+    .then((products) => {
+      const product = products[0];
       if (!product) {
         return res.redirect("/");
       }
@@ -85,7 +90,8 @@ exports.postDeleteProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
