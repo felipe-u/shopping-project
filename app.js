81ff2a7e986776d6e8163d49c3a7c8fd404057f7
felipe-u <felipe_uv@outlook.com>
2024-08-30 19:40:05 -0500

    Syncing JS models

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const bodyParser = require("body-parser");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-const errorController = require('./controllers/error')
+const errorController = require("./controllers/error");
+const sequelize = require("./util/database");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -22,4 +22,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
