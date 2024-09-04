85395e104f1d2354bd1a9768256ab3bbcf0de886
felipe-u <felipe_uv@outlook.com>
2024-09-04 18:03:29 -0500

    Being able to delete products from the Cart

--- models/user.js
+++ models/user.js
@@ -48,6 +48,14 @@ userSchema.methods.addToCart = function (product) {
   return this.save();
 };
 
+userSchema.methods.deleteFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter((item) => {
+    return item.product.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
+
 module.exports = mongoose.model("User", userSchema);
 
 // const getDb = require("../util/database").getDb;
